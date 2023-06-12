bbbe6ba500f90eb506c27aac66bf6335ac2f55b7
bravene <brian.ravenet@universite-paris-saclay.fr>
2023-06-12 23:58:05 +0200

    ajout du projet

---
+++ MinesweeperWPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MinesweeperWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private UIElement GetUIElementFromPosition(Grid g, int col, int row)
+        {
+            return g.Children.Cast<UIElement>().First(e => Grid.GetRow(e) == row && Grid.GetColumn(e) == col);
+        }
+    }
+}
